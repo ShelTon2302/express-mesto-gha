fae2a1f27cab2a5d6de700b2e9ca5f17f3e04793
Anton Shelanov <shelanov.anton@yandex.ru>
2022-09-17 01:02:58 +0300

    PR13 before 1 review

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/', router, (req, res, next) => {
-  res.status(404).send({ message: 'Запрашиваемый пользователь не найден' });
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
 app.listen(PORT);
