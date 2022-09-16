93fc2be3030e32e5cac172d85d40eab8d22120a2
Anton Shelanov <shelanov.anton@yandex.ru>
2022-09-17 01:02:20 +0300

    PR13 before 1 review

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   //useFindAndModify: false
 });
 
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
   req.user = {
@@ -24,6 +24,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/', router);
+app.use('/', router, (req, res, next) => {
+  res.status(404).send({ message: 'Запрашиваемый пользователь не найден' });
+});
 
 app.listen(PORT);
