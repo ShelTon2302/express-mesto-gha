3fff326b5b0112b060cb60fdcda71a4fd4571b8d
Шеланов Антон Алексеевич <BT144@bunker-trade.local>
2022-09-27 19:58:27 +0300

    PR14 errors

--- routes/card.js
+++ routes/card.js
@@ -9,7 +9,7 @@ cardRouter.get('/cards', getCards);
 cardRouter.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().uri(),
+    link: Joi.string().required().regex(/(^https?:\/\/)(www.)?[a-z0-9-]+\.[a-z]{2,9}[a-z0-9\-\\._~:\\/?#\\[\]@!$&'\\(\\)*\\+,;=]/),
   }),
 }), createCard);
 
@@ -17,13 +17,13 @@ cardRouter.delete('/cards/:cardid', deleteCard);
 
 cardRouter.put('/cards/:cardid/likes', celebrate({
   params: Joi.object().keys({
-    id: Joi.string().required().length(24),
+    cardid: Joi.string().required().length(24),
   }),
 }), likeCard);
 
 cardRouter.delete('/cards/:cardid/likes', celebrate({
   params: Joi.object().keys({
-    id: Joi.string().required().length(24),
+    cardid: Joi.string().required().length(24),
   }),
 }), dislikeCard);
 
